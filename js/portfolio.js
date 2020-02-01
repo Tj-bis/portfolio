44367add3aca4413aa33d70ae95951d5c6028696
Cecman <jane.trajkoski@gmail.com>
2020-02-01 19:16:26 +0100

    Adding JQuery

---
+++ js/portfolio.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 800) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+});
