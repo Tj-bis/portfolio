7add1473157d3260e38d520ae84f4d4f8ee0f098
Cecman <jane.trajkoski@gmail.com>
2020-02-04 13:39:39 +0100

    Updates

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 var btn = $('#button');
 
 $(window).scroll(function() {
-  if ($(window).scrollTop() > 800) {
+  if ($(window).scrollTop() > 500) {
     btn.addClass('show');
   } else {
     btn.removeClass('show');
@@ -11,5 +11,20 @@ $(window).scroll(function() {
 btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
+});
+$('.flip').click(function(){
+  $(this).find('.card').toggleClass('flipped');
+});
+
+$('#toggle').on('click' ,function(){
+  $('.card').toggleClass('flipped');
+});
+
+$('#front').on('click' ,function(){
+  $('.card').addClass('flipped');
+});
+
+$('#back').on('click' ,function(){
+  $('.card').removeClass('flipped');
 });
 });
